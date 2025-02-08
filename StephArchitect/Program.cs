26eb6b6db4cb3d827b6eaa56e46e25931c9f65e7
Joanita Nell <joanitan@agilebridge.co.za>
2025-02-08 12:01:43 +0200

    progress

--- StephArchitect/Program.cs
+++ StephArchitect/Program.cs
@@ -3,18 +3,18 @@
 var projectName = "Bruno";
 
 // mac
-//var baseOutputPath = $"/Users/stephankroukamp/RiderProjects/{projectName}";
-//var inputFilePath = "/Users/stephankroukamp/RiderProjects/StephArchitect/StephArchitect/Input/example.json";
-
-var baseOutputPath = $"../../Output/{projectName}";
-var inputFilePath = "Input/example.json";
+var baseOutputPath = $"/Users/joanitanell/Documents/GitHub/{projectName}";
+var inputFilePath = "/Users/joanitanell/Documents/GitHub/StephArchitect/StephArchitect/Input/example.json";
 
 // windows
 // var baseOutputPath = @$"C:\\Projects\\{projectName}\\Api";
 // var inputFilePath = @"C:\\Projects\\StephArchitect\\StephArchitect\\Input\\example.json";
 
-var apiGenerator = new ApiProjectGenerator(projectName, $"{baseOutputPath}-API", inputFilePath);
-await apiGenerator.GenerateFromInput();
+// var apiGenerator = new ApiProjectGenerator(projectName, $"{baseOutputPath}-API", inputFilePath);
+// await apiGenerator.GenerateFromInput();
+
+var frontendGenerator = new FrontendProjectGenerator(projectName, baseOutputPath, inputFilePath);
+await frontendGenerator.GenerateFromInput();
 
 // var mobileGenerator = new MobileProjectGenerator(projectName, $"{StringExtensions.ToSnakeCase(baseOutputPath)}-mobile", inputFilePath);
 // await mobileGenerator.GenerateFromInput();
