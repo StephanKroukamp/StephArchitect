5f1e2f566452fceb4440ff9f0151562bead92b82
Danie Leeuwner <DanieL@agilebridge.co.za>
2025-02-08 10:08:10 +0200

    Add mobile generator

--- StephArchitect/Program.cs
+++ StephArchitect/Program.cs
@@ -1,15 +1,19 @@
 ﻿using StephArchitect;
+using System.IO;
 
 var projectName = "Bruno";
 
 // mac
-var baseOutputPath = $"/Users/stephankroukamp/RiderProjects/{projectName}/Api";
-var inputFilePath = "/Users/stephankroukamp/RiderProjects/StephArchitect/StephArchitect/Input/example.json";
+var baseOutputPath = $"../../Output/{projectName}";
+var inputFilePath = "Input/example.json";
 
 // windows
 // var baseOutputPath = @$"C:\\Projects\\{projectName}\\Api";
 // var inputFilePath = @"C:\\Projects\\StephArchitect\\StephArchitect\\Input\\example.json";
 
-var generator = new ApiProjectGenerator(projectName, baseOutputPath, inputFilePath);
-
+var generator = new ApiProjectGenerator(projectName, Path.Join(baseOutputPath, "API"), inputFilePath);
 await generator.GenerateFromInput();
+
+
+var mobileGenerator = new MobileProjectGenerator(projectName, $"{StringExtensions.ToSnakeCase(baseOutputPath)}-mobile", inputFilePath);
+await mobileGenerator.GenerateFromInput();
